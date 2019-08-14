d795e4398005e9135770109a8d7378bfd328c1cc
15463 <1546352238@qq.com>
2019-08-15 03:31:17 +0800

    下载功能test

--- app/src/main/java/com/fr/konwledge/base/BaseActivity.java
+++ app/src/main/java/com/fr/konwledge/base/BaseActivity.java
@@ -22,6 +22,12 @@ public abstract class BaseActivity<V extends ViewDataBinding> extends AppCompatA
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
+
+        //防止App长时间进入后台，再次进入导致崩溃
+        if (savedInstanceState != null) {
+            savedInstanceState.putParcelable("android:fragments", null);
+        }
+
         super.onCreate(savedInstanceState);
 
         SystemUtil.setStatusBarCharacterDarkColor(this);
@@ -33,31 +39,34 @@ public abstract class BaseActivity<V extends ViewDataBinding> extends AppCompatA
         initData();
     }
 
-    private void initViewDataBinding(Bundle savedInstanceState){
-        binding = DataBindingUtil.setContentView(this,initContentView(savedInstanceState));
+    private void initViewDataBinding(Bundle savedInstanceState) {
+        binding = DataBindingUtil.setContentView(this, initContentView(savedInstanceState));
 
     }
+
     protected abstract void initData();
 
     public abstract int initContentView(Bundle savedInstanceState);
 
     /**
      * 跳转页面
+     *
      * @param clz 所跳转目的的Activity类
      */
-    public void startActivity(Class clz){
-            startActivity(new Intent(this,clz));
+    public void startActivity(Class clz) {
+        startActivity(new Intent(this, clz));
     }
 
     /**
      * 跳转页面
-     * @param clz 所跳转目的的Activity类
+     *
+     * @param clz    所跳转目的的Activity类
      * @param bundle 跳转所携带的信息
      */
-    public void startActivity(Class clz,Bundle bundle){
-        Intent intent = new Intent(this,clz);
-        if (bundle != null){
-            intent.putExtra("bundle",bundle);
+    public void startActivity(Class clz, Bundle bundle) {
+        Intent intent = new Intent(this, clz);
+        if (bundle != null) {
+            intent.putExtra("bundle", bundle);
         }
         startActivity(intent);
     }
