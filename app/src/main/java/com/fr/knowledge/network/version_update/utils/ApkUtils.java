2d1254f7a94ee44459bfbb107d98878ab2abb79e
15463 <1546352238@qq.com>
2019-08-18 00:37:08 +0800

    应用内更新功能完成

--- app/src/main/java/com/fr/knowledge/network/version_update/utils/ApkUtils.java
+++ app/src/main/java/com/fr/knowledge/network/version_update/utils/ApkUtils.java
@@ -37,7 +37,7 @@ public class ApkUtils {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
-            Uri uri = FileProvider.getUriForFile(context, "com.fr.konwledge.provider", apkFile);
+            Uri uri = FileProvider.getUriForFile(context, "com.fr.knowledge.provider", apkFile);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             intent.setDataAndType(uri, "application/vnd.android.package-archive");
         } else {
